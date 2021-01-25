109b71825995afa2413521b0f15f7630b0657cf0
Luke Underwood <veryveryeasily@gmail.com>
2021-01-24 19:48:08 -0800

    Kind of working!!!

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,40 @@
 <template>
   <div>
     <div id="app"></div>
-    <svg width="660" height="220" @click="handleClick($event)">
-      <defs>
-        <linearGradient
-          id="linear"
-          :x1="percentage(point1)"
-          :y1="percentage(point1)"
-          :x2="percentage(point2)"
-          :y2="percentage(point2)"
-        >
-          <stop offset="0%" stop-color="#05a" />
-          <stop offset="100%" stop-color="#0a5" />
-        </linearGradient>
-      </defs>
+    <svg
+      ref="svg"
+      width="660"
+      height="220"
+      viewBox="0 0 660 220"
+      @click="handleClick($event)"
+    >
+      <template v-if="svgReady">
+        <defs>
+          <linearGradient
+            id="linear"
+            :x1="percentage(points.startPoint).x"
+            :y1="percentage(points.startPoint).y"
+            :x2="percentage(points.endPoint).x"
+            :y2="percentage(points.endPoint).y"
+          >
+            <stop offset="0%" stop-color="#05a" />
+            <stop offset="100%" stop-color="#0a5" />
+          </linearGradient>
+        </defs>
 
-      <polyline :points="polygonPoints" fill="url(#linear)" />
+        <polyline :points="polygonPoints" fill="url(#linear)" />
+      </template>
     </svg>
+    <button
+      type="button"
+      style="margin-top: 1rem;"
+      @click="
+        points.currentTarget =
+          points.currentTarget === 'startPoint' ? 'endPoint' : 'startPoint'
+      "
+    >
+      Click me to change the target point
+    </button>
   </div>
 </template>
 
@@ -24,8 +42,13 @@
 export default {
   data() {
     return {
-      point1: { x: 0, y: 0 },
-      point2: { x: 600, y: 100 },
+      svgReady: false,
+      targetPoint: "startPoint",
+      points: {
+        currentTarget: "startPoint",
+        startPoint: { x: 0, y: 0 },
+        endPoint: { x: 600, y: 100 },
+      },
       polygon: {
         brush: { size: 5, color: "#ff0000" },
         points: [
@@ -42,16 +65,46 @@ export default {
       return this.polygon.points.map((point) => point.join(",")).join(" ");
     },
   },
+  mounted() {
+    this.svgReady = Boolean(this.$refs.svg);
+  },
+  updated() {
+    this.svgReady = Boolean(this.$refs.svg);
+  },
   methods: {
     percentage(point) {
-      return {
-        x: (660 / point.x) * 100 + "%",
-        y: (220 / point.x) * 100 + "%",
+      const temp = {
+        x: (point.x / 660) * 100 + "%",
+        y: (point.y / 220) * 100 + "%",
       };
+      console.log("percentage", temp);
+      return temp;
     },
     handleClick(e) {
-      console.log("handleClick", e);
-      this.point1 = { x: e.clientX, y: e.clientY };
+      const bounds = this.$refs.svg.getBoundingClientRect();
+      const point = {
+        x: e.clientX - bounds.left,
+        y: e.clientY - bounds.top,
+      };
+      console.log("handleClick", e, point);
+      this.$set(this.points, this.points.currentTarget, point);
+    },
+    svgPoint() {
+      return this.$refs.svg.createSVGPoint();
+    },
+    // screenPoint needs to be an object with Number "x" and "y" properties.
+    toSvgPoint(screenPoint) {
+      const svg = this.$refs.svg;
+
+      const svgPoint = svg.createSVGPoint();
+      svgPoint.x = screenPoint.x;
+      svgPoint.y = screenPoint.y;
+
+      const matrix = svg.getScreenCTM();
+      const cursorpt = svgPoint.matrixTransform(matrix.inverse());
+
+      console.log("(" + cursorpt.x + ", " + cursorpt.y + ")");
+      return cursorpt;
     },
   },
 };
