2980b91beb5057d783987edfea24fe8b42745be5
Luke Underwood <veryveryeasily@gmail.com>
2021-01-24 20:30:08 -0800

    Dump SVG to bitmap on the browser

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
   <div>
-    <div id="app"></div>
-    <svg
-      ref="svg"
-      width="660"
-      height="220"
-      viewBox="0 0 660 220"
-      @click="handleClick($event)"
-    >
-      <template v-if="svgReady">
-        <defs>
-          <linearGradient
-            id="linear"
-            :x1="percentage(points.startPoint).x"
-            :y1="percentage(points.startPoint).y"
-            :x2="percentage(points.endPoint).x"
-            :y2="percentage(points.endPoint).y"
-          >
-            <stop offset="0%" stop-color="#05a" />
-            <stop offset="100%" stop-color="#0a5" />
-          </linearGradient>
-        </defs>
+    <div class="wrapper">
+      <canvas ref="canvas" class="canvas" />
+      <div class="screen" />
+      <svg
+        ref="svg"
+        width="660"
+        height="220"
+        viewBox="0 0 660 220"
+        @click="handleClick($event)"
+        class="svg"
+      >
+        <template v-if="svgReady">
+          <defs>
+            <linearGradient
+              id="linear"
+              :x1="percentage(points.startPoint).x"
+              :y1="percentage(points.startPoint).y"
+              :x2="percentage(points.endPoint).x"
+              :y2="percentage(points.endPoint).y"
+            >
+              <stop offset="0%" stop-color="#05a" />
+              <stop offset="100%" stop-color="#0a5" />
+            </linearGradient>
+          </defs>
 
-        <polyline :points="polygonPoints" fill="url(#linear)" />
-      </template>
-    </svg>
+          <polyline :points="polygonPoints" fill="url(#linear)" />
+        </template>
+      </svg>
+    </div>
     <button
       type="button"
       style="margin-top: 1rem;"
@@ -35,15 +39,24 @@
     >
       Click me to change the target point
     </button>
+    <button type="button" style="margin-top: 1rem;" @click="dumpSvgToBitmap">
+      Click me to dump svg to bitmap
+    </button>
+    <div v-if="svgBitmap" class="bitmap">
+      {{ svgBitmap }}
+    </div>
   </div>
 </template>
 
 <script>
+import Canvg from "canvg";
+
 export default {
   data() {
     return {
       svgReady: false,
       targetPoint: "startPoint",
+      svgBitmap: null,
       points: {
         currentTarget: "startPoint",
         startPoint: { x: 0, y: 0 },
@@ -91,6 +104,15 @@ export default {
       this.$set(this.points, this.points.currentTarget, point);
     },
 
+    async dumpSvgToBitmap() {
+      const canvas = this.$refs.canvas;
+      const ctx = canvas.getContext("2d");
+      const svgHtml = this.$refs.svg.outerHTML;
+      const canvg = new Canvg.fromString(ctx, svgHtml);
+      await canvg.render();
+      this.svgBitmap = canvas.toDataURL("image/png");
+    },
+
     // NOTE: Not currently used but will probably eventually be useful. This
     // converts screen coordinates to SVG coordinates.
     //
@@ -115,13 +137,47 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.wrapper {
+  position: absolute;
+  width: 660px;
+  height: 220px;
+  position: relative;
+}
+
+.svg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+}
+
+.canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.screen {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  background-color: white;
+}
+
+.bitmap {
+  margin-top: 1rem;
+  margin-left: 1rem;
+  border: 2px solid #888;
+  max-width: 500px;
+  word-break: break-all;
+  overflow-wrap: break-word;
 }
 </style>
